ab9c367d20893bf5650595b1353a3278a0717d14
Sotelo27 <lmsotelo@fi.uba.ar>
2021-07-22 15:33:01 -0300

    descarga_archivos

--- funcionalidades_gmail.py
+++ funcionalidades_gmail.py
@@ -1,56 +1,65 @@
 from servicio_gmail import obtener_servicio
 import base64
-import os
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
-from email.mime.base import MIMEBase
-from email.mime.audio import MIMEAudio
-from email.mime.image import MIMEImage
-import mimetypes
+from apiclient import errors
 servicio = obtener_servicio()
-
-def crear_mensaje_con_adjunto(sender, to, subject, message_text, file):
-    message = MIMEMultipart()
-    message['to'] = to
-    message['from'] = sender
-    message['subject'] = subject
-
-    msg = MIMEText(message_text)
-    message.attach(msg)
-
-    (content_type, encoding) = mimetypes.guess_type(file)
-
-    if content_type is None or encoding is not None:
-        content_type = 'application/octet-stream'
-    (main_type, sub_type) = content_type.split('/', 1)
-    if main_type == 'text':
-        with open (file, 'rb') as f:
-            msg = MIMEText(f.read().decode('utf-8'),_subtype = sub_type)
-    elif main_type == 'image':
-        with open (file, 'rb') as f:
-            msg = MIMEImage(f.read(), _subtype=sub_type)
-        
-    elif main_type == 'audio':
-        with open (file, 'rb') as f:
-            msg = MIMEAudio(f.read(), _subtype=sub_type)
-    else:
-        with open (file,'rb') as f:
-            msg = MIMEBase(main_type, sub_type)
-            msg.set_payload(f.read())
-    filename = os.path.basename(file)
-    msg.add_header('Content-Disposition', 'attachment', filename=filename)
-    message.attach(msg)
-    raw_msg = base64.urlsafe_b64encode(message.as_string().encode('utf-8'))
+def crear_correo(remitente:str, destinatario:str, asunto:str, texto_mensaje:str)->object:
+    mensaje = MIMEText(texto_mensaje)
+    mensaje['to'] = destinatario
+    mensaje['from'] = remitente
+    mensaje['subject'] = asunto
+    raw_msg = base64.urlsafe_b64encode(mensaje.as_string().encode('utf-8'))
     return {'raw':raw_msg.decode('utf-8')}
 
-def enviar_correo(service, usuario_id, message):
+def enviar_correo(servicio:object, usuario_id:str, mensaje:object):
     try:
-        message = service.users().messages().send(userId=usuario_id, body=message).execute()
+        message = servicio.users().messages().send(userId=usuario_id, body=mensaje).execute()
         print ('Message Id: {}'.format(message['id']))
         return message
     except Exception as error:
         print('An error occurred: {}'.format(error))
         return None
 
+def definir_errores():
+    pass
 
+def buscar_email_con_adjunto(servicio,cadena_string,etiquetas_id):
+    try:
+        lista_mensaje= servicio.users().messages().list(userId='me',labelIds=etiquetas_id,q=cadena_string).execute()
+        items_mensajes = lista_mensaje.get('messages')
+        nextPageToken = lista_mensaje.get('nextPageToken')
+        while nextPageToken:
+            lista_mensaje = servicio.user().messages().list(userId = 'me',labelIds=etiquetas_id,q=cadena_string,pageToken=nextPageToken).execute()
+            items_mensajes.extend(lista_mensaje.get('messages'))
+            nextPageToken = items_mensajes.get('nextPageToken')
+        return items_mensajes
+    except Exception as e:
+        return None
+def detalles_del_email(servicio,id_mensaje,format='metadata',metadata_headers=[]):
+    try:
+        detalles_mensaje = servicio.users().messages().get(userId='me',id=id_mensaje,format=format,metadataHeaders=metadata_headers).execute()
+        return detalles_mensaje
+    except Exception as e:
+        print(e)
+        return None
+def descargar_adjunto(service,user_id,msg_id,store_dir=''):
+    try:
+        message = service.users().messages().get(userId='me', id=msg_id).execute()
+
+        for part in message['payload']['parts']:
+            newvar = part['body']
+        if 'attachmentId' in newvar:
+            att_id = newvar['attachmentId']
+            att = service.users().messages().attachments().get(userId=user_id, messageId=msg_id, id=att_id).execute()
+            data = att['data']
+            file_data = base64.urlsafe_b64decode(data.encode('UTF-8'))
+            print(part['filename'])
+            path = ''.join([store_dir, part['filename']])
+            f = open(path, 'wb')
+            f.write(file_data)
+            f.close()
+    except errors.HttpError as error:
+        print ('An error occurred: {%s}'.format(error))
+        
 
