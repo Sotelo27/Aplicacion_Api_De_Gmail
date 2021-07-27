5ab659781a8c8beb9e13bb43bf701ce00b3752e7
Sotelo27 <lmsotelo@fi.uba.ar>
2021-07-27 19:53:17 -0300

    filtros_busqueda

--- funcionalidades_gmail.py
+++ funcionalidades_gmail.py
@@ -3,6 +3,7 @@ import base64
 from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from apiclient import errors
+import pickle,os
 servicio = obtener_servicio()
 def crear_correo(remitente:str, destinatario:str, asunto:str, texto_mensaje:str)->object:
     mensaje = MIMEText(texto_mensaje)
@@ -61,5 +62,59 @@ def descargar_adjunto(service,user_id,msg_id,store_dir=''):
             f.close()
     except errors.HttpError as error:
         print ('An error occurred: {%s}'.format(error))
+
+def crear_filtro(servicio,usuario_id,cuerpo):
+    filtro = servicio.users().settings().filters().create(userId = usuario_id,body = cuerpo).execute()
+    print('Filtro creado {}'.format(filtro.get('id')))
+    return filtro
+
+def obtener_hilos_de_consulta(servicio,usuario_id,etiqueta_id,consulta):
+    respuesta = servicio.users().threads().list(userId = usuario_id,labelIds = etiqueta_id,q = consulta).execute()
+    threads = []
+    if 'threads' in respuesta:
+        threads.extend(respuesta['threads'])
+
+    while 'nextPageToken' in respuesta:
+        pageToken = respuesta['nextPageToken']
+        respuesta = servicio.users().threads().list(userId = usuario_id,labelIds = etiqueta_id,q = consulta,pageToken = pageToken).execute()
+        threads.extend(respuesta['threads'])
+
+    return threads
+
+def crear_busqueda_consulta(criterio_busqueda):
+    lista_de_consulta = []
+    llaves_mensaje = ["from","to","subject"]
+    for llaves in llaves_mensaje:
+        valores = criterio_busqueda.get(llaves)
+        if valores is not None:
+            lista_de_consulta.append("("+llaves+":"+valores+")")
+    valores = criterio_busqueda.get("query")
+    if valores is not None:
+        lista_de_consulta.append("("+valores+")")
+
+    return " AND ".join(lista_de_consulta)
+def aplicar_filtro_a_busqueda(servicio,usuarioId,filtro_objeto):
+    consulta = crear_busqueda_consulta(filtro_objeto["criteria"])
+    hilos = obtener_hilos_de_consulta(servicio,usuarioId,[],consulta)
+    aniadir_etiquetas = filtro_objeto["action"]["addLabelIds"]
+    print("Etiquetas aniadidas {} a {} hilos".format(aniadir_etiquetas,len(hilos)))
+    for contenido in hilos:
+        cuerpo = {"addLabelIds":aniadir_etiquetas,"removeLabelIds":[]}
+        servicio.users().threads().modify(userId=usuarioId,id=contenido["id"],body=cuerpo).execute()
         
+def busqueda_filtros(servicio):
+    consulta = input('Introduzca un filtro de busqueda: ')
+    filtro_deseado = [{"criteria": {"subject": "[JIRA]",},"action": {"addLabelIds": [me["jira"]],}},]
+    for contenido in filtro_deseado:
+        crear_filtro(servicio,"me",contenido)
+        aplicar_filtro_a_busqueda(servicio,"me",contenido)
+
+def main():
+    remitente ='lmsotelo@fi.uba.ar'
+    destinatario = ''
+    texto_mensaje = 'Su entrega ha sido correcta'
+    texto_mensaje_error = 'Su entrega no a sido enviada correctamente'
+    asunto = 'Entrega evaluacion'
+    correo = crear_correo(remitente,destinatario,asunto,texto_mensaje)
+    directorio = input('Ingrese el directorio en el que desee descargar el archivo')
 
